2c43c7414a83f2baa85b5761b4ad1ddaeb894df9
Davi Viana <davianasales@outlook.com>
2023-02-22 08:15:37 -0300

    chore: install mdi

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
